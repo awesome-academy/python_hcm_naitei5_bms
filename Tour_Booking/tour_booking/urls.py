636e4432de377fc54a6fc12b9440e97dc6afe346
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-07-26 15:43:48 +0700

    Create Login/Logout
    ready

--- Tour_Booking/tour_booking/urls.py
+++ Tour_Booking/tour_booking/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tours/', views.TourListView.as_view(), name='tours'),
     path('tour/<int:pk>', views.TourDetailView.as_view(), name='tour-detail'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Tour_Booking/tour_booking/views.py
+++ Tour_Booking/tour_booking/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext
 from django.views import generic
 from .models import Tour
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -15,3 +15,22 @@ class TourListView(generic.ListView):
 
 class TourDetailView(generic.DetailView):
     model = Tour
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  
+        else:
+            return render(request, 'login.html', {'error_message': 'Đăng nhập không thành công. Vui lòng thử lại.'})
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
