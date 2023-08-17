5e3c8f07476d309feee87852ed5e1a12a66d9709
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-08-17 10:29:48 +0700

    API Favorite_Tour

--- Tour_Booking/api/urls.py
+++ Tour_Booking/api/urls.py
@@ -3,4 +3,6 @@ from . import api_views
 
 urlpatterns = [
     path('login/', api_views.login_view, name='api-login'),
+    path('toggle-favorite/<int:tour_id>/', api_views.toggle_favorite_tour, name='toggle-favorite-tour'),
+    path('favorite-tours/', api_views.FavoriteToursListView.as_view(), name='favorite-tours-list'),
 ]
