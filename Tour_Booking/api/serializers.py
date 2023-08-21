85f1e4cb693345d15a2bb0a3a81b47209fee584f
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-08-21 09:35:08 +0700

    API Comment, Reply, thống kê Admin

--- Tour_Booking/api/serializers.py
+++ Tour_Booking/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
-from tour_booking.models import Tour, FavoriteTour
+from tour_booking.models import Tour, FavoriteTour, Booking, Rating, Reply
 from rest_framework import serializers
 
 class UserSerializer(ModelSerializer):
@@ -18,6 +18,21 @@ class FavoriteTourSerializer(ModelSerializer):
         model = FavoriteTour
         fields = ["id","name"]
 
+class RatingSerializer(ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ["user","tour","rating","content"]
+
+class ReplySerializer(ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ["user","content","parent_comment"]
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ["user","tour","number_of_people","price","departure_date","end_date"]
+
 class ChangeUserInfoSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     first_name = serializers.CharField(required=False)
@@ -30,3 +45,28 @@ class ChangeUserInfoSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password1 = serializers.CharField(required=True)
     new_password2 = serializers.CharField(required=True)
+
+class RevenueStatisticsSerializer(serializers.Serializer):
+    total_revenue = serializers.DecimalField(max_digits=10, decimal_places=2)
+    tour_name = serializers.CharField(source='tour.name')
+
+    fields = ["total_revenue", "tour_name"]
+
+class BookingRatioStatisticsSerializer(serializers.Serializer):
+    total_booking = serializers.IntegerField()
+    tour_details = TourSerializer(many=True)
+
+    def get_tour_details(self, obj):
+        tour_details = []
+        for tour in obj['tour_details']:
+            booking_count = Booking.objects.filter(tour=tour).count()
+            tour_details.append({
+                "tour": TourSerializer(tour).data,
+                "booking_count": booking_count
+            })
+        return tour_details
+
+class MonthlyRevenueSerializer(serializers.Serializer):
+    tour_name = serializers.CharField()
+    monthly_revenue = serializers.DecimalField(max_digits=10, decimal_places=2)
+    total_bookings = serializers.IntegerField()
--- Tour_Booking/api/urls.py
+++ Tour_Booking/api/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('toggle-favorite/<int:tour_id>/', api_views.toggle_favorite_tour, name='toggle-favorite-tour'),
     path('favorite-tours/', api_views.FavoriteToursListView.as_view(), name='favorite-tours-list'),
     path('change-user-info/', api_views.ChangeUserInfoAPIView.as_view(), name='api-change-user-info'),
+    path('<int:tour_id>/submit-rating/', api_views.submit_rating_api, name='submit-rating-api'),
+    path('submit-reply-comment/<int:pk>/', api_views.submit_reply_comment_api, name='submit-reply-comment-api'),
+    path('revenue-statistics/', api_views.revenue_statistics, name='revenue-statistics'),
+    path('booking-ratio-statistics/', api_views.booking_ratio_statistics, name='booking-ratio-statistics'),
+    path('monthly-revenue-statistics/', api_views.monthly_revenue_statistics, name='monthly-revenue-statistics'),
 ]
